1098c98aa034b2da3a75c4549012933fa0bf4c86
Ryan Brennan <ryanbrennan@ryans-mbp-2.gateway.sonic.net>
2021-08-22 19:32:38 -0700

    date values coming through on event

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -20,37 +20,11 @@ export class CalendarComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // isInvalidDate(date) {
-  //   return date.weekday() === 0;
-  // }
 
-  // onChange(e) {
-  //   console.log('I am the event ',e)
-  // }
-
-  // isCustomDate(date) {
-  //   return date.weekday() === 0 || date.weekday() === 6
-  //     ? 'mycustomdate'
-  //     : false;
-  // }
+  addEvent(e, event) {
+    console.log('whattt ', event)
+  }
 }
 
 
 
-
-
-// @Component({
-//   selector: 'datepicker-min-max-example',
-//   templateUrl: 'datepicker-min-max-example.html',
-// })
-// export class DatepickerMinMaxExample {
-//   minDate: Date;
-//   maxDate: Date;
-
-//   constructor() {
-//     // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
-//     const currentYear = new Date().getFullYear();
-//     this.minDate = new Date(currentYear - 20, 0, 1);
-//     this.maxDate = new Date(currentYear + 1, 11, 31);
-//   }
-// }
