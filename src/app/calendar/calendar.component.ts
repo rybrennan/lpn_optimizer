94f6713ee526cb2ad9ec520bd5a8126a6439586e
Ryan Brennan <ryanbrennan@ryans-mbp-2.gateway.sonic.net>
2021-08-23 20:34:45 -0700

    implement ngfor on days

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import * as moment from 'moment';
 
 @Component({
@@ -7,24 +8,67 @@ import * as moment from 'moment';
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
-  minDate: Date;
-  maxDate: Date;
+  currentDay: Date;
+  currentYear: number;
+  currentMonth: number;
+  months: Object;
+  days: Object;
+  date: number = 1;
+  firstDay: number;
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private _document: Document) {
     // this.alwaysShowCalendars = true;
     const currentYear = new Date().getFullYear();
-    this.minDate = new Date(currentYear - 20, 0, 1);
-    this.maxDate = new Date(currentYear + 1, 11, 31);
-
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentDay = new Date();
+    this.currentYear = new Date().getFullYear();
+    this.currentMonth = this.currentDay.getMonth();
+    this.months = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    this.days = [
+      'Sun', 'Mon', 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat'
+    ]
 
+    console.log('Heyyy', this.showCalendar(1, 2021));
+  }
+
+  showCalendar(month: any, year: number) {
+    let firstDay = new Date(year, month).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+    console.log('Days in Month ', daysInMonth);
+  }
+
+  createCells(firstDay: number, daysInMonth: number) {
+    for (let i = 0; i < 6; i++) {
+      const row = this._document.createElement('tr');
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          const cell = this._document.createElement('tr');
+          const cellText = this._document.createTextNode('');
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else if (this.date > daysInMonth) {
+
+        }
+      }
+    }
+  }
 
   addEvent(e, event) {
-    console.log('whattt ', event)
+    console.log('whattt ', event);
   }
 }
-
-
-
